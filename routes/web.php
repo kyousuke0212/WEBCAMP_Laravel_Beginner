51945930367ebfecc04d107cca33dcea7f646aae
tomioka kyousuke <tomioka kyousuke@example.com>
2023-03-07 12:56:40 +0000

    非ログインTopPageとログイン後TopPageを作成

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,7 +17,7 @@ use App\Http\Controllers\AuthController;
 */
 // タスク管理システム
 Route::get('/', [AuthController::class, 'index']);
-
+Route::get('/task/list', [TaskController::class, 'list']);
 
 // テスト用
 Route::get('/welcome', [WelcomeController::class, 'index']);
