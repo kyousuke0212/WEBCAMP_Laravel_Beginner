00f21faa36fd4465b8816bcb10688ff936aa12a9
tomioka kyousuke <tomioka kyousuke@example.com>
2023-03-20 11:14:31 +0000

    エラー

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,9 @@
 declare(strict_types=1);
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\LoginPostRequest;
+use app\Http\Controllers\Controller;
+use app\Http\Requests\LoginPostRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -27,8 +28,18 @@ class AuthController extends Controller
 
         // データの取得
         $datum = $request->validated();
+        //var_dump($datum); exit;
+
+        // 認証
+        if (Auth::attempt($datum) === false) {
+            return back()
+                   ->withInput() // 入力値の保持
+                   ->withErrors(['auth' => 'emailかパスワードに誤りがあります。',]) // エラーメッセージの出力
+                   ;
+        }
 
         //
-        var_dump($datum); exit;
+        $request->session()->regenerate();
+        return redirect()->intended('/task/list');
     }
 }
