e4414c35c8eb8fdab203dcf804cd1944c81853e9
tomioka kyousuke <tomioka kyousuke@example.com>
2023-03-22 07:25:59 +0000

    認証と認可の実装まで書いた

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,18 @@
- @extends('layout')
-    
- {{-- メインコンテンツ --}}
+@extends('layout')
+
+{{-- メインコンテンツ --}}
 @section('contets')
-     <h1>ログイン</h1>
+        <h1>ログイン</h1>
+        @if ($errors->any())
+            <div>
+            @foreach ($errors->all() as $error)
+                {{ $error }}<br>
+            @endforeach
+            </div>
+        @endif
         <form action="/login" method="post">
             @csrf
-            email：<input name="email"><br>
+            email：<input name="email" value="{{ old('email') }}"><br>
             パスワード：<input  name="password" type="password"><br>
             <button>ログインする</button>
         </form>
